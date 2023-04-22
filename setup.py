459a09eaf5f2327f309abceb90d94c5c58ea5b3c
parth-agarwal-aera <parth.agarwal@aeratechnology.com>
2023-04-22 21:00:35 +0530

    Second Commit-Setup.py and req.txt

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This finction will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path,'r') as file_obj:
+        req = file_obj.readlines()
+        req = [x.replace('\n','') for x in req]
+        req.remove('-e .')
+    
+    return req
+
+
+
+setup(
+    name = 'MLPROJECT',
+    version = '0.0.1',
+    author = 'Parth',
+    author_email = 'parth.agarwal@aeratechnology.com',
+    packages = find_packages(),
+    install_requires = get_requirements('requirements.txt')
+
+)
